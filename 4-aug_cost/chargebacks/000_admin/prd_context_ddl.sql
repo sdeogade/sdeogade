cadb86ff22140e9c1178e73b3ba37a0ee9a849f0
Cloud User <ec2-user@ip-172-31-19-148.us-west-2.compute.internal>
2021-09-22 13:47:04 +0000

    changed PRD to DEV in 000_admin/prd_context_ddl.sql

--- 4-aug_cost/chargebacks/000_admin/prd_context_ddl.sql
+++ 4-aug_cost/chargebacks/000_admin/prd_context_ddl.sql
@@ -3,16 +3,16 @@
 --
 !print set environment context
 
-!define l_common_db=PRD_PLAT_GOV_COMMON_DB
+!define l_common_db=DEV_PLAT_GOV_COMMON_DB
 !define l_common_schema=UTIL
 
-!define l_raw_db=PRD_PLAT_GOV_RL_DB
+!define l_raw_db=DEV_PLAT_GOV_RL_DB
 !define l_raw_schema=account_usage
 
-!define l_il_db=PRD_PLAT_GOV_IL_DB
+!define l_il_db=DEV_PLAT_GOV_IL_DB
 !define l_il_schema=USAGE_METRIC
 
-!define l_pl_db=PRD_PLAT_GOV_PL_DB
+!define l_pl_db=DEV_PLAT_GOV_PL_DB
 !define l_pl_schema=REPORTING
 
 !print SUCCESS!
